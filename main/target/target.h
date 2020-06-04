0e3ebc1a80f4edd9e0a00fdb29a4c1cb24fb0cb1
Paul Kurucz <pkurucz@gmail.com>
2020-06-04 08:07:15 -0400

    P2P appears to be broken with the ESP IDF 4.0  Troubleshoot me...

--- main/target/target.h
+++ main/target/target.h
@@ -2,14 +2,14 @@
 
 #if defined(ESP32)
 // All ESP32 targets support these features
-#define USE_P2P
+//#define USE_P2P
 #define USE_BLUETOOTH
 #define USE_OTA
 #define USE_DEVELOPER_MENU
 #define USE_IDF_WMONITOR
 #endif
 
-#include "platform.h"
+#include "raven_platform.h"
 
 #if defined(HAVE_SDKCONFIG_H)
 #include <sdkconfig.h>
