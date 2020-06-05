0a7ecd09205b83b8c3721d8b71db5a6992710020
Paul Kurucz <pkurucz@gmail.com>
2020-06-05 12:43:52 -0400

    Upates to support the TTGO V2 board and a WS2812 LED

--- main/target/platforms/esp32_lora_ttgo_oled_v2_868_915/raven_platform.h
+++ main/target/platforms/esp32_lora_ttgo_oled_v2_868_915/raven_platform.h
@@ -3,4 +3,10 @@
 #include "target/platforms/esp32_lora_ttgo_v2/common.h"
 #include "target/platforms/esp32_lora_ttgo_v2/screen.h"
 
+//#define LED_1_GPIO 25
+//#define LED_1_USE_PWM
+
+#define LED_1_GPIO 0
+#define LED_1_USE_WS2812
+
 #define BOARD_NAME "ESP32+LoRa TTGO V2 868/915MHz"
